6a5d3c1e6e7611aaca8f1ad2d3b3d1c93b54b40d
Bence Deli <delibnc@gmail.com>
2020-08-02 09:19:05 +0200

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Forza Horizon 4 LED tachometer
+You can set in the Forza Horizon settings, to send telemetry through UDP packet to a defined ip and port. I write a program that recives all the UDP packets and get the RPM informations (current RPM, idle RPM, max RPM) and with them i represent the current RPM value with 8 leds through a shift register.
+
+In action footage: https://www.youtube.com/watch?v=HXDwOsYU45Q
+
+# Enable 'Data out' in Forza Horizon 4
+Settings -> HUD and Gameplay -> DATA OUT set to 'ON' and set 'DATA OUT IP ADDRESS' and 'DATA OUT IP ADDRESS'
+
+# Developed in
+C++ with PlatformIO (VS Code)
+
+# Used libraries
+- AsyncUDP form (https://github.com/espressif/arduino-esp32)
+- ShiftRegister74HC595 (https://github.com/Simsso/ShiftRegister74HC595)
+
+# Wiring
+![led_tacho](led_tacho_bb.png)
---
+++ src/main.cpp
@@ -0,0 +1,119 @@
+#include <Arduino.h>
+#include <ShiftRegister74HC595.h> //https://github.com/Simsso/ShiftRegister74HC595
+#include <WiFi.h>
+#include <AsyncUDP.h> //https://github.com/espressif/arduino-esp32
+#include <cmath> //for std::round
+
+const char* ssid = "WiFi hotspot name";
+const char* password = "passwd";
+
+const int udp_port = 1101; //port to listen to
+
+// parameters: <number of shift registers> (data pin, clock pin, latch pin)
+ShiftRegister74HC595<1> sr(16, 17, 18);
+AsyncUDP udp;
+
+float idle_rpm = 0.0;
+float current_rpm = 0.0;
+float max_rpm = 0.0;
+
+
+unsigned long last_packet_in = 0;
+
+void connectWifi(){
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(WiFi.localIP());
+
+}
+
+void setTachometer(){
+  if(current_rpm == 0.0 && max_rpm == 0.0 && idle_rpm == 0.0){
+    sr.setAllLow();
+  }else{
+    float cr = current_rpm - idle_rpm;
+    float mr = max_rpm - idle_rpm;
+
+    float cr_percentage = cr / mr; //between 0 and 1
+
+    int no_leds = round(8 * cr_percentage);
+
+    for (int i = 0; i < 8; i++) {
+      if(i < no_leds){
+        sr.set(i, HIGH);
+      }else
+      {
+        sr.set(i, LOW);
+      }
+    }
+  }
+  sr.updateRegisters();
+
+
+}
+
+void listenUDP(int port){
+  if(udp.listen(port)) {
+        Serial.print("UDP Listening on IP: ");
+        Serial.println(WiFi.localIP());
+        udp.onPacket([](AsyncUDPPacket packet) {
+          if (packet.length() == 324 ) {
+            char rpm[4]; // four bytes in a float 32
+
+            //current rpm
+            rpm[0] = packet.data()[16];
+            rpm[1] = packet.data()[17];
+            rpm[2] = packet.data()[18];
+            rpm[3] = packet.data()[19];
+            
+            current_rpm = *( (float*) rpm);
+            
+            //idle rpm
+            rpm[0] = packet.data()[12];
+            rpm[1] = packet.data()[13];
+            rpm[2] = packet.data()[14];
+            rpm[3] = packet.data()[15];
+            if( idle_rpm != *( (float*) rpm)){
+              idle_rpm = *( (float*) rpm);
+            }
+          
+            //max rpm
+            rpm[0] = packet.data()[8];
+            rpm[1] = packet.data()[9];
+            rpm[2] = packet.data()[10];
+            rpm[3] = packet.data()[11];
+            if( max_rpm != *( (float*) rpm) ){
+              max_rpm = *( (float*) rpm);
+            }
+            if(current_rpm > 0.1){
+              setTachometer();
+              last_packet_in = millis();
+            }
+            
+          }
+        });
+    }
+}
+
+void setup() {
+  Serial.begin(115200);
+  connectWifi();
+  listenUDP(udp_port);
+  sr.setAllLow();
+  sr.updateRegisters();
+}
+
+void loop() {
+  unsigned long current_millis = millis();
+
+  if(current_millis - last_packet_in >= 1000 && current_rpm <= 0.1){
+    current_rpm = 0.0;
+    max_rpm = 0.0;
+    idle_rpm = 0.0;
+    setTachometer();
+  }
+}
